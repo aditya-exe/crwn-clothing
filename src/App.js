815b104a373150a74dc8d51214800f92dc6b6900
aditya-exe <47419610+aditya-exe@users.noreply.github.com>
2021-08-03 13:19:20 +0530

    Fixed bugs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import {Route, Switch} from "react-router-dom";
+import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import Header from "./components/header/header.component";
 import {auth} from './firebase/firebase.utils'
-import './App.css';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
 
         this.state = {
             currentUser: null
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -15,10 +15,10 @@ const config = {
 firebase.initializeApp(config)
 
 export const auth = firebase.auth()
-export const firestore=firebase.firestore()
+export const firestore = firebase.firestore()
 
-const provider= new firebase.auth.GoogleAuthProvider()
-provider.setCustomParameters({prompt: 'select-account'})
-export const signInWithGoogle=()=> auth.signInWithPopup(provider)
+const provider = new firebase.auth.GoogleAuthProvider()
+provider.setCustomParameters({prompt: 'select_account'})
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase
