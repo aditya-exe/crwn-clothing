decb8a129157d005eb682ba08ce525242a163962
aditya-exe <47419610+aditya-exe@users.noreply.github.com>
2021-08-11 20:18:43 +0530

    Updating the application with reselect

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,15 +1,22 @@
 import CartActionTypes from './cart.types'
+import {addItemToCart} from "./cart.util";
 
 const INITIAL_STATE = {
-    hidden: true
+    hidden: true,
+    cartItems: []
 }
 
-const cartReducer = (state = INITIAL_STATE, action)=>{
-    switch (action.type){
+const cartReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
         case CartActionTypes.TOGGLE_CART_HIDDEN:
-            return{
+            return {
                 ...state,
                 hidden: !state.hidden
+            }
+        case CartActionTypes.ADD_ITEM:
+            return {
+                ...state,
+                cartItems: addItemToCart(state.cartItems, action.payload)
             }
         default:
             return state
