60d08fe8b1129baab91dbceae1e23c82ba6856a5
aditya-exe <47419610+aditya-exe@users.noreply.github.com>
2022-04-24 17:54:35 +0530

    seprated server and frontend code

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import StripeCheckout from "react-stripe-checkout"
+import axios from 'axios'
 
 const StripeCheckoutButton = ({price}) => {
     const priceForStripe = price * 100
     const publishableKey = 'pk_test_51KDmGPSFjfuSWJo8kvFeY9KKyzzxTav9D3vWUNUvLxiGhUZBDYcfEJ0N658NL7EMRqWzksTXLh5UFZVvlZ207Gqd00JwloW1PD'
 
     const onToken = token => {
-        console.log(token)
-        alert('Payment Successful')
+        axios({
+            url:'payment',
+            method:'post',
+            data:{
+                amount:priceForStripe,
+                token
+            }
+        }).then(response=>{
+            alert('Payment Successful')
+        }).catch(error=>{
+            console.log('Payment error: ', JSON.parse(error))
+            alert('here was an issue with the payment. Please make sure you use the provided credit card')
+        })
     }
 
     return (
