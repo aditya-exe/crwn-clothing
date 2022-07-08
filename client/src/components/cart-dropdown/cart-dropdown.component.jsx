864194daf5b1996f717176cf4ad6b576eb055243
aditya <pokemaniacaditya@gmail.com>
2022-07-09 01:26:43 +0530

    updated modules 2

--- client/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ client/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {createStructuredSelector} from 'reselect';
-import {withRouter} from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { withRouter } from 'react-router-dom';
 
 import CartItem from '../cart-item/cart-item.component';
-import {selectCartItems} from '../../redux/cart/cart.selectors';
-import {toggleCartHidden} from '../../redux/cart/cart.actions.js';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { toggleCartHidden } from '../../redux/cart/cart.actions.js';
 
 import {
     CartDropdownButton,
@@ -14,12 +14,12 @@ import {
     EmptyMessageContainer
 } from './cart-dropdown.styles';
 
-const CartDropdown = ({cartItems, history, dispatch}) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
     <CartDropdownContainer>
         <CartItemsContainer>
             {cartItems.length ? (
                 cartItems.map(cartItem => (
-                    <CartItem key={cartItem.id} item={cartItem}/>
+                    <CartItem key={cartItem.id} item={cartItem} />
                 ))
             ) : (
                 <EmptyMessageContainer>Your cart is empty</EmptyMessageContainer>
